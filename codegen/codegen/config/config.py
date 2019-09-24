4853c8e4efb6ccb15709a4f5c20e321a8323c44b
Marcin Weglowski <marcin@escali.no>
2019-09-24 15:14:46 +0200

    remove dest_folder from config

--- codegen/codegen/config/config.py
+++ codegen/codegen/config/config.py
@@ -1,9 +1,8 @@
 class config(object):
-    def __init__(self, source_path, source_tab, dest_folder, code_items, start_search_row, column_range_start, column_range_stop):
+    def __init__(self, source_path, source_tab, code_items, start_search_row, column_range_start, column_range_stop):
         self.source_path        = source_path
         self.source_tab         = source_tab
         self.code_items         = code_items
-        self.dest_folder        = dest_folder
         self.start_search_row   = start_search_row
         self.column_range_start = column_range_start
         self.column_range_stop  = column_range_stop
