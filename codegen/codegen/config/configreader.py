53779089908a581e2dc0127421981b4d3bd3a37b
Marcin Weglowski <marcin@escali.no>
2019-10-03 08:55:26 +0200

    Another formatting option

--- codegen/codegen/config/configreader.py
+++ codegen/codegen/config/configreader.py
@@ -15,7 +15,7 @@ def read():
     source_tabs         = json_content['source_tabs']
     start_search_row    = json_content['start_search_row']
     column_range_start  = json_content['column_range_start']
-    column_range_stop    = json_content['column_range_stop']
-    code_items       = [code_item(**item) for item in json_content['code_items']]
+    column_range_stop   = json_content['column_range_stop']
+    code_items       = [code_item(**item) for item in json_content['code_items'] if item['is_active'] == True]
     config_data      = config.config(source_path, source_tabs, code_items, start_search_row, column_range_start, column_range_stop)
     return config_data
--- codegen/codegen/core/codeitem.py
+++ codegen/codegen/core/codeitem.py
@@ -1,24 +1,26 @@
 class code_item(object):
-    def __init__(self, source_tab, dest_file, start_key, start_offset, end_key, line_pattern):
+    def __init__(self, source_tab, dest_file, start_key, start_offset, end_key, line_pattern, is_active=True):
         self.source_tab   = source_tab
         self.dest_file    = dest_file
         self.start_key    = start_key
         self.start_offset = start_offset
         self.end_key      = end_key
         self.line_pattern = line_pattern
+        self.is_active    = is_active
 
     def generate_code(self, dataset):
         column_field = 0
         column_id    = 1
         column_type  = 2
         return [self.line_pattern.format(field_name        = row[column_field].value, 
-                                    field_name_quoted = "\"{}\"".format(row[column_field].value), 
-                                    id                = row[column_id].value, 
-                                    data_type         = row[column_type].value,
-                                    field_name_field  = "{}Field".format(row[column_field].value),
-                                    field_name_short  = "{}Short".format(row[column_field].value),
-                                    field_name_descr  = "{}Description".format(row[column_field].value),
-                                    field_id          = 485 + row[column_id].value * 3 + 0,
-                                    short_id          = 485 + row[column_id].value * 3 + 1,
-                                    descr_id          = 485 + row[column_id].value * 3 + 2)
+                                         field_name_quoted = "\"{}\"".format(row[column_field].value), 
+                                         id                = row[column_id].value, 
+                                         data_type         = row[column_type].value,
+                                         field_name_field  = "{}Field".format(row[column_field].value),
+                                         field_name_short  = "{}Short".format(row[column_field].value),
+                                         field_name_descr  = "{}Description".format(row[column_field].value),
+                                         field_id          = 485 + row[column_id].value * 3 + 0,
+                                         short_id          = 485 + row[column_id].value * 3 + 1,
+                                         descr_id          = 485 + row[column_id].value * 3 + 2,
+                                         offset_const      = "offset + " if row[column_id].value > 0 else "")
                 for row in dataset[self.source_tab]]
