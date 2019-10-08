676cd20215bac2557fea9bc9798c24b44da80fc5
Marcin Weglowski <marcin@escali.no>
2019-10-08 16:22:52 +0200

    Generate HasKey() property in configuration

--- codegen/codegen_dbfirst/core/destination_item.py
+++ codegen/codegen_dbfirst/core/destination_item.py
@@ -9,17 +9,21 @@ class destination_item(object):
 
     def get_code(self, table):
         print("Table code: {}".format(table.table_name))
-        columns_text = [self.get_row(column) for column in table.columns]
-        file_content = self.get_file_content(table, columns_text)
+        columns_text = "".join([self.get_row(column) for column in table.columns])
+        primary_key = self.get_primary_key(table.columns)
+        file_content = self.get_file_content(table, columns_text, primary_key)
         return file_content
 
-    def get_file_content(self, table, columns_text):
+    def get_file_content(self, table, columns_text, primary_key):
         return self.file_pattern.format(entity_name  = table.entity_name, 
                                         table_name   = table.table_name, 
                                         columns_text = columns_text,
-                                        primary_key  = "PK_COLUMN")
+                                        primary_key  = primary_key)
 
     def get_row(self, column):
         return self.line_pattern.format(csharp_type = sqltocsharp.get_type(column.data_type, column.is_nullable),
                                         field_name = column.name,
                                         configuration = sqltocsharp.get_configuration(column.data_type, column.is_nullable, column.max_length, column.primary_key))
+
+    def get_primary_key(self, columns):
+        return "".join([column.name for column in columns if column.primary_key == True])
